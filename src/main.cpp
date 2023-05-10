2ab833a824008fbda1d471c298fb1abc4bc52868
birbaubin <aubin.birba@gmail.com>
2023-05-09 23:53:32 -0400

    Working on version based on elliptic curves

--- src/main.cpp
+++ src/main.cpp
@@ -1,65 +1,87 @@
-#include "main.h"
-
-using namespace std;
-
-
-int main(int argc, char** argv)
-{
-
-        const int symsecbits = 80;
-        const int number_of_selected_nodes = 5;
-
-        crypto crypto(symsecbits);
-	pk_crypto* field = crypto.gen_field((field_type)0);
-
-        string dataset_name = "email.csv";
-
-        string network1_name = "datasets/net1-"+dataset_name; 
-        string network2_name = "datasets/net2-"+dataset_name; 
-
-        uint32_t graph_1_size, graph_2_size = 0;
-
-        vector<UndirectedEdge> graph1 = load_graph(&graph_1_size, network1_name);
-        vector<UndirectedEdge> graph2 = load_graph(&graph_2_size, network2_name);
-
-        vector<uint32_t> selected_nodes = select_random_node(graph1, number_of_selected_nodes);
-
-        vector<UndirectedEdge> evaluated_graph = generate_complete_graph(selected_nodes);
+#include <iostream>
+#include <openssl/bn.h>
+#include <openssl/rand.h>
+#include <ctime>
+#include <openssl/err.h>
+#include <openssl/ec.h>
+#include <openssl/obj_mac.h>
 
 
-        // vector<uint32_t> neighbors_nodex_1 = neighbors(graph1, nodex);
-        // int number_neighbors_nodex_1 = neighbors_nodex_1.size();
-        // vector<uint32_t> neighbors_nodey_1 = neighbors(graph1, nodey);
-        // int number_neighbors_nodey_1 = neighbors_nodey_1.size();
-        // vector<uint32_t> neighbors_nodex_2 = neighbors(graph2, nodex);
-        // int number_neighbors_nodex_2 = neighbors_nodex_2.size();
-        // vector<uint32_t> neighbors_nodey_2 = neighbors(graph2, nodey);
-        // int number_neighbors_nodey_2 = neighbors_nodey_2.size();
-
-
-        // cout << "Number of neighbors of node x in graph 1 = " << number_neighbors_nodex_1 << endl;
-        // cout << "Number of neighbors of node y in graph 1 = " << number_neighbors_nodey_1 << endl;
-        // cout << "Number of neighbors of node x in graph 2 = " << number_neighbors_nodex_2 << endl;
-        // cout << "Number of neighbors of node y in graph 2 = " << number_neighbors_nodey_2 << endl;
-
-
-        run_clear_protocol(evaluated_graph, graph1, graph2);
-
-        // run_baseline_protocol_inline(evaluated_graph, graph1, graph2, field);
-
-        run_baseline_protocol_inline2(evaluated_graph, graph1, graph2, field);
-
-        // run_new_protocol_inline(evaluated_graph, graph1, graph2, field, false);
-
-        run_new_protocol_inline(evaluated_graph, graph1, graph2, field, true);
-
-
-        // print_graph(evaluated_graph);
-
-
-        // cout << edge_exists(graph1, edge);
-
-	return 1;
+void print_bn(const char *label, BIGNUM *bn) {
+    char *str = BN_bn2dec(bn);
+    std::cout << label << ": " << str << std::endl;
+    OPENSSL_free(str);
 }
 
 
+void print_ec_point(const EC_GROUP *group, const EC_POINT *point) {
+    BN_CTX *ctx = BN_CTX_new();
+    char *point_hex = EC_POINT_point2hex(group, point, POINT_CONVERSION_UNCOMPRESSED, ctx);
+    printf("EC Point: %s\n", point_hex);
+    OPENSSL_free(point_hex);
+    BN_CTX_free(ctx);
+}
+
+
+int main() {
+    // Initialize OpenSSL components
+
+    EC_GROUP *group;
+    EC_POINT *base, *result;
+    BN_CTX *ctx;
+
+
+    OpenSSL_add_all_algorithms();
+    ERR_load_crypto_strings();
+
+    // Seed the random number generator
+    srand(static_cast<unsigned int>(time(NULL)));
+    RAND_poll();
+
+    group = EC_GROUP_new_by_curve_name(NID_secp256k1);
+    base = EC_POINT_new(group);
+    result = EC_POINT_new(group);
+    EC_POINT_copy(base, EC_GROUP_get0_generator(group));
+
+
+    // Create BIGNUM context
+    ctx = BN_CTX_new();
+
+    // Create modulus 2^256
+    BIGNUM *modulus = BN_new();
+    BN_set_bit(modulus, 256);
+
+    // Generate random BIGNUM
+    BIGNUM *alpha = BN_new();
+    BN_rand_range(alpha, modulus);
+
+    BIGNUM *beta = BN_new();
+    BN_rand_range(beta, modulus);
+
+//    // Calculate the inverse modulo 2^256
+//    BIGNUM *inverse = BN_mod_inverse(NULL, alpha, modulus, ctx);
+
+    EC_POINT_mul(group, result, NULL, base, alpha, ctx);
+    EC_POINT_mul(group, result, NULL, result, beta, ctx);
+
+    print_ec_point(group, result);
+
+    EC_POINT_mul(group, result, NULL, base, beta, ctx);
+    EC_POINT_mul(group, result, NULL, result, alpha, ctx);
+
+    print_ec_point(group, result);
+
+
+    BN_free(alpha);
+//    BN_free(inverse);
+    BN_free(modulus);
+    BN_CTX_free(ctx);
+    EC_POINT_free(base);
+    EC_POINT_free(result);
+    EC_GROUP_free(group);
+    // Clean up OpenSSL components
+    EVP_cleanup();
+    ERR_free_strings();
+
+    return 0;
+}
